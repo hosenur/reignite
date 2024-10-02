f77732f66e6f16abba31e3400adf0ac61c4d1e48
hosenur <hosenur.dev@gmail.com>
2024-10-02 18:12:49 +0530

    fixed package versions

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), tailwind()],
 });
